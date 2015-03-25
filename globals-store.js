25e37339a3598969cd7ae5ff86bb2b51c12394bc
Vito Tardia <vito@tardia.me>
2015-03-25 13:53:38 +0100

    Store driver passes all basic and close tests

--- globals-store.js
+++ globals-store.js
@@ -3,12 +3,13 @@
 "use strict";
 
 var globals = require('./lib/cache');
-var _       = require('lodash')
+var _       = require('lodash');
 
-var name = "globals-store"
+var name = "globals-store";
 
 
 module.exports = function(options) {
+
   var seneca = this;
   var desc;
 
@@ -19,17 +20,17 @@ module.exports = function(options) {
     username: '_SYSTEM',
     password: 'SYS',
     namespace: 'USER',
-  },options)
+  }, options);
 
-  function error(args,err,cb) {
+  function error(args, err, cb) {
     if( err ) {
-      seneca.log.error('entity',err,{store:name})
+      seneca.log.error('entity', err, {store:name});
       return true;
     }
     else return false;
   }
 
-	function configure(params,cb) {
+	function configure(params, cb) {
 		myData = new globals.Cache();
 
 		myData.open(
@@ -48,27 +49,88 @@ module.exports = function(options) {
 		);
 	}
 
+  function getGlobalName(ent) {
+    var canon = ent.canon$({object:true});
+
+    var global = (canon.base ? canon.base + '.' : '') + canon.name;
+
+    return global;
+  }
+
+  function removeById(global, id, cb) {
+    myData.get({
+      global: global,
+      subscripts: [id]
+    }, function (err, result) {
+      if (err) {
+        seneca.log.error('entity',result.ErrorMessage || 'remove error',{id:id});
+      }
+      if (_.isEmpty(result.data) || 0 === result.defined) {
+        cb(err, null );
+      } else {
+
+        myData.kill({global: global, subscripts: [id]}, function(err, result){
+          cb(err);
+        });
+
+      }
+    });
+  }
+
+  function removeByQuery(global, q, cb) {
+
+      myData.retrieve({
+        global: global
+      }, 'list', function (err, result) {
+        if( !err || 0 === err ) {
+
+          result.forEach(function (item) {
+            if ('counter' !== item) {
+              var result = myData.get({global:global, subscripts:[item]});
+              if (!_.isEmpty(result.data)) {
+                var data = JSON.parse(result.data);
+                if (Object.keys(q).length > 0) {
+                  var matches = false;
+                  for(var key in q) { 
+                     if (data.hasOwnProperty(key) && data[key] == q[key]) {
+                      matches = true;
+                     } else {
+                      matches = false;
+                     }
+                  }
+                  if (true === matches) {
+                      // remove the first item and exit with callback
+                      var res = myData.kill(global, data.id);
+                      if (0 === res) {
+                        cb(null, null);
+                      }
+                  }
+                }
+              }
+            }
+          });          
+        }
+        cb(err);
+      });
+  }
+
   var store = {
     name:name,
 
-    close: function(args,cb) {
+    close: function(args, cb) {
       if(myData) {
-        myData.close(cb)
+        myData.close(cb);
       }
       else return cb();
     },
 
-    save: function(args,cb) {
+    save: function(args, cb) {
 
-      var ent = args.ent
-      var global = args.name;
+      var ent = args.ent;
 
-      if (args.base) {
-        global = args.base + '.' + global;
-      }
+      var global = getGlobalName(ent);
 
       var update = !!ent.id;
-      seneca.log.info('entity','save start',{store:name,args:args});
 
       if( !update  ) {
 
@@ -80,21 +142,18 @@ module.exports = function(options) {
         }
       }
 
-      var object = {
-        node: {
-          global: global,
-          subscripts: [ent.id]
-        },
-        object: {}
-      };
+      var object = {};
       var fields = ent.fields$()
       fields.forEach( function(field) {
-        object.object[field] = ent[field]
+        object[field] = ent[field];
       })
-      seneca.log.info('entity','save',{store:name,object:object});
-      myData.update(object, 'object', function (err, result) {
+
+      myData.set({
+        global: global,
+        subscripts: [ent.id],
+        data: JSON.stringify(object)
+      }, function (err, result) {
         if( !error(args,err,cb) ) {
-          seneca.log.info('entity','result',{store:name,result:result});
           cb(err, ent);
         }
       });
@@ -103,80 +162,124 @@ module.exports = function(options) {
 
     load: function(args,cb) {
       // the entity
-      var qent = args.qent
+      var qent = args.qent;
 
       // the query object (eg. {id:'some-id'})
-      var q    = args.q
+      var q    = args.q;
 
       // the entity name, that maps to GlobalsDB container (eg. a table)
-      var global = args.name;
-
-      if (args.base) {
-        global = args.base + '.' + global;
-      }
+      var global = getGlobalName(qent);
 
       var ent = null;
 
-      seneca.log.info('entity',args,{store:name,args:args})
-
-      myData.retrieve({
-        global: global,
-        subscripts: [q.id]
-      }, 'object', function (err, result) {
-        if (err) {
-          seneca.log.error('entity',result.ErrorMessage || 'load error',{store:name,args:args})
-        }
-        if (_.isEmpty(result.object)) {
-          cb(err, null );
-        } else {
-          ent = qent.make$(result.object);
-          cb(err, ent ? ent : null );
-        }
-      });
+      if (q.id) {
+        myData.get({
+          global: global,
+          subscripts: [q.id]
+        }, function (err, result) {
+          if (err) {
+            seneca.log.error('entity',result.ErrorMessage || 'load error',{store:name,args:args});
+          }
+          if (_.isEmpty(result.data) || 0 === result.defined) {
+            cb(err, null );
+          } else {
+            ent = qent.make$(JSON.parse(result.data));
+            cb(err, ent ? ent : null );
+          }
+        });
+      } else {
+        // Call list method and return the first of the list to callback, if not error
+        store.list(args, error(cb, function(list){
+          cb(null, list[0])
+        }));
+      }
 
     },
 
     list: function(args,cb) {
-      var global = args.name;
-      var qent = args.qent
+      var qent = args.qent;
+      var global = getGlobalName(qent);
+      var q = args.q;
 
-      if (args.base) {
-        global = args.base + '.' + global;
-      }
-
-      seneca.log.info('entity','list start',{store:name,args:args});
       myData.retrieve({
         global: global
       }, 'list', function (err, result) {
 
         var list = [];
 
-        if( !err || 0 == err ) {
+        if( !err || 0 === err ) {
 
           result.forEach(function (item) {
 
             if ('counter' !== item) {
-              var obj = myData.retrieve({global:global, subscripts:[item]}, 'object');
-              seneca.log.info('entity','list',{store:name,result:obj});
-              if (!_.isEmpty(obj)) {
-                var ent = qent.make$(obj.object);
-                seneca.log.info('entity','ent',{store:name,ent:ent});
-                list.push(ent);
+              var result = myData.get({global:global, subscripts:[item]});
+              if (!_.isEmpty(result.data)) {
+                var ent = qent.make$(JSON.parse(result.data));
+                if (Object.keys(q).length > 0) {
+                  var matches = false;
+                  for(var key in q) { 
+                      console.log(key);
+                     if (ent.hasOwnProperty(key) && ent[key] === q[key]) {
+                      matches = true;
+                     } else {
+                      matches = false;
+                     }
+                  }
+                  if (true === matches) {
+                      list.push(ent);
+                  }
+                } else {
+                  // no query, retrieving all objects
+                  list.push(ent);
+                }
               }
 
             }
-            cb(null, list);
-          });
 
+          });
+          cb(null, list);
         }
-        
+
       });
     },
 
-    remove: function(args,cb) {
+    remove: function(args, cb) {
+      var qent = args.qent
+      var q    = args.q
+
+      var all  = q.all$ // default false
+      var load  = _.isUndefined(q.load$) ? true : q.load$ // default true
+
+      var global = getGlobalName(qent);
+
+      if (all) {
+        myData.kill({global: global}, function(err, result){
+          cb(err);
+        });
+      } else {
+        // List and delete
+        if (q.id) {
+          removeById(global, q.id, function(err){
+            if (!err || 0 == err) {
+              cb(null, null);
+            } else {
+              cb(err);
+            }
+          });
+        } else {
+          removeByQuery(global, q, function(err){
+            if (!err || 0 == err) {
+              cb(null, null);
+            } else {
+              cb(err);
+            }
+          });
+        }
+      }
     },
 
-    native: function(args,done) {
+    native: function(args, done) {
+      done(null, myData);
     }
   }
 
@@ -184,13 +287,13 @@ module.exports = function(options) {
   desc = meta.desc;
 
 
-  seneca.add({init:store.name,tag:meta.tag},function(args,done){
-    configure(options,function(err){
-      if( err ) return seneca.die('store',err,{store:store.name,desc:desc});
+  seneca.add({init:store.name, tag:meta.tag}, function(args, done){
+    configure(options, function(err){
+      if( err ) return seneca.die('store', err, {store:store.name, desc:desc});
       return done();
     })
   })
 
-  return {name:store.name,tag:meta.tag}
+  return {name:store.name, tag:meta.tag};
 
 }
